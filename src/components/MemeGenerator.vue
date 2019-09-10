fda227c73f94fd1769f580acf654ed89da5e8b66
renangabriel27 <renanthompsom@gmail.com>
2019-09-10 10:28:51 -0300

    1. Hello Vue

---
+++ src/components/MemeGenerator.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    {{ message }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MemeGenerator',
+
+  data() {
+    return {
+      message: 'Hello Vue!'
+    };
+  },
+}
+</script>
